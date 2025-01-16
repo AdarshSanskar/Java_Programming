04990a9d0234201acda53559a7aaf825371393d0
AdarshTile <astile1424@gmail.com>
2025-01-16 17:21:35 +0530

    Commit Test

--- src/StarPattern.java
+++ src/StarPattern.java
@@ -1,5 +1,14 @@
-public class StarPattern {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+public class StarPattern
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0 ; i < 5 ; i++)
+        {
+            for(int j = i ; j >= 5 ; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
