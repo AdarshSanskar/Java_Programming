ced19aca88eb819f62c35c35c6ab52b1a1536bd1
AdarshTile <astile1424@gmail.com>
2025-02-17 18:45:29 +0530

    6:45 Commit

---
+++ src/Pr6_04.java
@@ -0,0 +1,10 @@
+public class Pr6_04
+{
+    public static void main(String[] args)
+    {
+        String str = "123";
+        Integer num = Integer.valueOf(str);
+        System.out.println("String : "+str);
+        System.out.println("Integer : "+num);
+    }
+}
---
+++ src/Pr6_2.java
@@ -0,0 +1,11 @@
+public class Pr6_2 // Unboxing
+{
+    public static void main(String[] args)
+    {
+        Integer x = Integer.valueOf(10);
+//        Integer x = 10;
+        int xp = x.intValue();
+        System.out.println("X : "+x);
+        System.out.println("XP : "+xp);
+    }
+}
