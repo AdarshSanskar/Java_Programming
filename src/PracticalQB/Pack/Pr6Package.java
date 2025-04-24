c23be917b753d7db25db49b915dab2cfd6794c93
AdarshTile <astile1424@gmail.com>
2025-04-24 18:23:25 +0530

    Practical Commit

---
+++ src/PracticalQB/Pack/Pr6Package.java
@@ -0,0 +1,7 @@
+package PracticalQB.Pack;
+
+public class Pr6Package {
+    public void putMessage(){
+        System.out.println("This is from Package...");
+    }
+}
---
+++ src/PracticalQB/Pr6.java
@@ -0,0 +1,8 @@
+package PracticalQB;
+import PracticalQB.Pack.*;
+public class Pr6 {
+    public static void main(String[] args) {
+        Pr6Package pkg = new Pr6Package();
+        pkg.putMessage();
+    }
+}
